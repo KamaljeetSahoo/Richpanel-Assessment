ac538048412c408404f966b0e16cd45cba123127
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2022-10-01 12:37:46 +0530

    Templates folder and URL routing initiated

---
+++ authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import loginView, registerView
+
+urlpatterns = [
+    path('register/', registerView, name='register'),
+    path('login/', loginView, name='login'),
+]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def registerView(request):
+    return render(request, 'authentication/register.html')
+
+def loginView(request):
+    return render(request, 'authentication/login.html')
