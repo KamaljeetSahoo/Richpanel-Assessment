dc1ccaad5030c219d11b9b61b2c44359ee4a3cc6
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2022-10-02 05:06:32 +0530

    Billing page added

--- subscription/views.py
+++ subscription/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render, redirect
+from .models import Plan
 
 # Create your views here.
 def billingView(request):
     if request.user.is_authenticated:
-        return render(request, 'subscription/billing.html')
+        plans = Plan.objects.all()
+        plan_types = []
+        prices =[]
+        video_qualities = []
+        resolutions = []
+        devices = []
+        for plan in plans:
+            plan_types.append(str(plan.plan_type))
+            prices.append(plan.monthly_price)
+            video_qualities.append(str(plan.video_quality))
+            resolutions.append(str(plan.resolution))
+            d = []
+            for device in plan.devices.all():
+                d.append(str(device))
+            devices.append(d)
+        
+        context = {
+            'plan_types': plan_types,
+            'prices': prices,
+            'video_qualities': video_qualities,
+            'resolutions': resolutions,
+            'devices': devices,
+        }
+        return render(request, 'subscription/billing.html', context=context)
     return redirect("login")
