89260bc8d76b67d817409afdcf679134b30f41c6
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2022-10-02 02:06:06 +0530

    Database Modelling of Subcriptions done

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import  PlanType, VideoQuality, Resolution, Device, Plan
 
 # Register your models here.
+admin.site.register(PlanType)
+admin.site.register(VideoQuality)
+admin.site.register(Resolution)
+admin.site.register(Device)
+admin.site.register(Plan)
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class PlanType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class VideoQuality(models.Model):
+    quality = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.quality
+
+class Resolution(models.Model):
+    resolution = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.resolution
+
+class Device(models.Model):
+    device_type = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self) -> str:
+        return self.device_type
+
+class Plan(models.Model):
+    plan_type = models.ForeignKey(PlanType, on_delete=models.CASCADE)
+    monthly_price = models.IntegerField()
+    yearly_price = models.IntegerField()
+    video_quality = models.ForeignKey(VideoQuality, on_delete=models.CASCADE)
+    resolution = models.ForeignKey(Resolution, on_delete=models.CASCADE)
+    devices = models.ManyToManyField(Device)
+
+    def __str__(self) -> str:
+        return str(self.plan_type)+'_'+str(self.video_quality)
