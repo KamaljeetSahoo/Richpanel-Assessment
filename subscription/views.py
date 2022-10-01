2b4173d2059291fd0e55f375f46297d5e09133f0
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2022-10-02 03:31:12 +0530

    URLs file for subscription app added

--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def billingView(request):
+    if request.user.is_authenticated:
+        return render(request, 'subscription/billing.html')
+    return redirect("login")
