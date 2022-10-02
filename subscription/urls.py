47b299b4cb4ad245c9bd80d6dec5419d2d0a686f
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2022-10-02 16:37:10 +0530

    Subcription details page added

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import billingView, paymentPage
+from .views import billingView, paymentPage, subscriptionPage, successPayment
 
 urlpatterns = [
     path('', billingView, name="billing"),
     path('paymentPage/<str:planType>/',paymentPage),
+    path('viewCurrentPlan/', subscriptionPage, name="currentPlan"),
+    path('successPay/<str:monthly>/<str:planType>/', successPayment),
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Plan, PlanType
 from decouple import config
 import stripe
+from datetime import datetime, timedelta
 stripe.api_key = config('STRIPE_API_KEY')
 
 # Create your views here.
@@ -45,16 +46,50 @@ def paymentPage(request, planType):
         context = {
             'client_secret': intent.client_secret,
             'amount': plan.monthly_price*100,
+            'planType': planType,
+            'monthly': 'monthly',
         }
         return render(request, 'subscription/paymentPage.html', context=context)
     return redirect('login')
 
+def successPayment(request, monthly, planType):
+    if request.user.is_authenticated:
+        print(monthly, planType)
+        selected_plan = PlanType.objects.get(name=planType)
+        plan = Plan.objects.get(plan_type=selected_plan)
+        if monthly == "monthly":
+            sub = request.user.usersubscription
+            sub.subscribed = True
+            sub.plan = plan
+            sub.cancelled = False
+            sub.subscription_date = datetime.now()
+            sub.expiration_date = datetime.now() + timedelta(30)
+            sub.save()
+        if monthly == "yearly":
+            sub = request.user.usersubscription
+            sub.subscribed = True
+            sub.plan = plan
+            sub.cancelled = False
+            sub.subscription_date = datetime.now()
+            sub.expiration_date = datetime.now() + timedelta(365)
+            sub.save()
+        return redirect("currentPlan")
+    return redirect("login")
+
 
 def subscriptionPage(request):
     if request.user.is_authenticated:
-        if request.user.usersubscription:
+        if request.user.usersubscription.subscribed:
+            time = request.user.usersubscription.expiration_date - request.user.usersubscription.subscription_date
+            print(time)
+            if str(time) == "30 days, 0:00:00":
+                monthly = 'monthly'
+            else:
+                monthly = 'yearly'
             context = {
                 'subscription': request.user.usersubscription,
+                'time' : time,
+                'monthly': monthly,
             }
             return render(request, 'subscription/viewPlan.html', context=context)
         else:
